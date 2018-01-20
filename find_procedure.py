18ea63c6711f93780dde73c2bcb7098012beaaca
Dina <dina.draguzya@gmail.com>
2018-01-20 23:50:53 +0200

    Refactored code

--- find_procedure.py
+++ find_procedure.py
@@ -1,13 +1,15 @@
 import os
 import re
 
+dir_name = 'Migrations'
 
-def find_files_by_text(target_dir, files_list, text):
+
+def find_files_by_text(files_list, text):
     search_result = list()
     file_match_count = 0
     for file in files_list:
         if file.endswith(".sql"):
-            with open(os.path.join(target_dir, file)) as opened_file:
+            with open(os.path.join(dir_name, file)) as opened_file:
                 data = opened_file.read()
                 if re.search(text, data, re.IGNORECASE):
                     search_result.append(file)
@@ -17,11 +19,15 @@ def find_files_by_text(target_dir, files_list, text):
     return search_result
 
 
+def recursive_search(files):
+    search_string = input('Enter a search string: ')
+    files_list = find_files_by_text(files, search_string)
+    if files_list:
+        recursive_search(files_list)
+
+
 if __name__ == '__main__':
-    migrations = 'Migrations'
     current_dir = os.path.dirname(os.path.abspath(__file__))
-    search_dir = os.path.join(current_dir, migrations)
+    search_dir = os.path.join(current_dir, dir_name)
     files_list = os.listdir(search_dir)
-    while True:
-        search_string = input('Enter a search string: ')
-        files_list = find_files_by_text(search_dir, files_list, search_string)
+    recursive_search(files_list)
